9041595d5a7505c3567eda81164dc13ea7f9b437
Maxim Kursakov <qmax.ku@gmail.com>
2023-11-09 22:45:48 +0300

    [hw02] main task solved

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,44 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var result strings.Builder
+	var repeatBuffer string
+	// var isBackslashed bool
+
+	if _, err := strconv.Atoi(str); err == nil {
+		return "", ErrInvalidString
+	}
+
+	for i, value := range str {
+		if i == 0 && unicode.IsDigit(value) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsLetter(value) {
+			result.WriteString(repeatBuffer)
+			repeatBuffer = string(value)
+		}
+
+		if unicode.IsDigit(value) {
+			if repeatBuffer == "" {
+				return "", ErrInvalidString
+			}
+			repeatCount, err := strconv.Atoi(string(value))
+			if err != nil {
+				return "", err
+			}
+			result.WriteString(strings.Repeat(repeatBuffer, repeatCount))
+			repeatBuffer = ""
+		}
+	}
+	result.WriteString(repeatBuffer)
+	return result.String(), nil
 }

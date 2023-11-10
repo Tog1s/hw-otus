907ea9446af0e76ad7a103fb260e6aa0ebfe297d
Maxim Kursakov <qmax.ku@gmail.com>
2023-11-10 09:22:44 +0300

    [hw02] additional task solved

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,7 +12,7 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(str string) (string, error) {
 	var result strings.Builder
 	var repeatBuffer string
-	// var isBackslashed bool
+	var isBackslashed bool
 
 	if _, err := strconv.Atoi(str); err == nil {
 		return "", ErrInvalidString
@@ -22,23 +22,45 @@ func Unpack(str string) (string, error) {
 		if i == 0 && unicode.IsDigit(value) {
 			return "", ErrInvalidString
 		}
+	
+		if isBackslashed {
+			if !(unicode.IsDigit(value) || string(value) == string('\\')) {
+				return "", ErrInvalidString
+			}
+			repeatBuffer = string(value)
+			isBackslashed = false
+			continue
+		}
+
+		if value == '\\' {
+			result.WriteString(repeatBuffer)
+			repeatBuffer = ""
+			isBackslashed = true
+			continue
+		}
 
 		if unicode.IsLetter(value) {
 			result.WriteString(repeatBuffer)
 			repeatBuffer = string(value)
+			continue
 		}
 
 		if unicode.IsDigit(value) {
 			if repeatBuffer == "" {
 				return "", ErrInvalidString
 			}
+
 			repeatCount, err := strconv.Atoi(string(value))
 			if err != nil {
 				return "", err
 			}
+
 			result.WriteString(strings.Repeat(repeatBuffer, repeatCount))
 			repeatBuffer = ""
 		}
+	}
+	if isBackslashed {
+		return "", ErrInvalidString
 	}
 	result.WriteString(repeatBuffer)
 	return result.String(), nil

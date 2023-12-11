1422ac9384e6ccb7bf962d15fdd0298e5b1d006d
Maxim Kursakov <qmax.ku@gmail.com>
2023-12-11 23:05:06 +0300

    [hw04] main task solved

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,83 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	len  int
+	head *ListItem
+	tail *ListItem
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (list *list) Len() int {
+	return list.len
+}
+
+func (list *list) Front() *ListItem {
+	return list.head
+}
+
+func (list *list) Back() *ListItem {
+	return list.tail
+}
+
+func (list *list) Remove(item *ListItem) {
+	if item == nil {
+		return
+	}
+
+	if item.Prev != nil {
+		item.Prev.Next = item.Next
+	} else {
+		list.head = item.Next
+	}
+
+	if item.Next != nil {
+		item.Next.Prev = item.Prev
+	} else {
+		list.tail = item.Prev
+	}
+	list.len--
+}
+
+func (list *list) MoveToFront(item *ListItem) {
+	if list.head == item {
+		return
+	}
+	list.Remove(item)
+	list.PushFront(item.Value)
+}
+
+func (list *list) PushFront(value interface{}) *ListItem {
+	newListItem := &ListItem{
+		Value: value,
+	}
+
+	if list.head == nil {
+		list.head = newListItem
+		list.tail = newListItem
+	} else {
+		newListItem.Next = list.head
+		list.head.Prev = newListItem
+		list.head = newListItem
+	}
+	list.len++
+	return list.head
+}
+
+func (list *list) PushBack(value interface{}) *ListItem {
+	newListItem := &ListItem{
+		Value: value,
+	}
+	if list.head == nil {
+		list.head = newListItem
+		list.tail = newListItem
+	} else {
+		newListItem.Prev = list.tail
+		list.tail.Next = newListItem
+		list.tail = newListItem
+	}
+	list.len++
+	return list.tail
 }

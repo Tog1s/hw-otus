bc99526e98e69130204af58b6aaf8684bf8cbf6b
Maxim Kursakov <qmax.ku@gmail.com>
2024-01-18 17:05:53 +0300

    [hw06] Add ExecutePipeline function

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,29 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	output := in
+	for _, stage := range stages {
+		output = func(in In) (out Out) {
+			valStream := make(Bi)
+			go func() {
+				defer close(valStream)
+				for {
+					select {
+					case <-done:
+						return
+					case v, ok := <-in:
+						if !ok {
+							return
+						}
+						select {
+						case valStream <- v:
+						case <-done:
+						}
+					}
+				}
+			}()
+			return stage(valStream)
+		}(output)
+	}
+	return output
 }

d7784ac1f306ac123feeb008f364213952ae0832
Maxim Kursakov <qmax.ku@gmail.com>
2024-01-24 15:59:32 +0300

    [hw07] basic Copy function

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -9,7 +11,56 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+// func checkFiles() {
+// 	return
+// }
+
+// func checkOffset() {
+// 	return
+// }
+
+func setLimit(limit int64, info os.FileInfo) int64 {
+	if limit <= 0 {
+		limit = info.Size()
+	}
+	return limit
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	// Read source file
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer sourceFile.Close()
+
+	fileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+	limit = setLimit(limit, fileInfo)
+
+	// Create target file
+	targetFile, err := os.Create(toPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer targetFile.Close()
+
+	// Offset
+	_, err = sourceFile.Seek(offset, io.SeekStart)
+	if errors.Is(err, io.EOF) {
+		err = nil
+	}
+	if err != nil {
+		return err
+	}
+
+	// Copy data
+	_, err = io.CopyN(targetFile, sourceFile, limit)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }

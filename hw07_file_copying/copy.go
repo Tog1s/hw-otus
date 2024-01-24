1476c433a294696b77bcb476d445231ff065a922
Maxim Kursakov <qmax.ku@gmail.com>
2024-01-24 16:54:05 +0300

    [hw07] add progress bar

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -4,20 +4,38 @@ import (
 	"errors"
 	"io"
 	"os"
+	"time"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrSameFileProvided      = errors.New("same file provided")
 )
 
-// func checkFiles() {
-// 	return
-// }
+func progressBarLimit(info os.FileInfo, limit, offset int64) int {
+	fileSize := info.Size()
+	if (fileSize - offset) < limit {
+		return int(fileSize - offset)
+	}
+	return int(limit)
+}
 
-// func checkOffset() {
-// 	return
-// }
+func checkFiles(fromPath, toPath string) error {
+	if fromPath == toPath {
+		return ErrSameFileProvided
+	}
+	return nil
+}
+
+func checkOffset(offset int64, info os.FileInfo) error {
+	if offset > info.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	return nil
+}
 
 func setLimit(limit int64, info os.FileInfo) int64 {
 	if limit <= 0 {
@@ -27,6 +45,12 @@ func setLimit(limit int64, info os.FileInfo) int64 {
 }
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
+	// Check files before
+	err := checkFiles(fromPath, toPath)
+	if err != nil {
+		return err
+	}
+
 	// Read source file
 	sourceFile, err := os.Open(fromPath)
 	if err != nil {
@@ -34,11 +58,18 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 	defer sourceFile.Close()
 
+	// Get file info
 	fileInfo, err := sourceFile.Stat()
 	if err != nil {
 		return err
 	}
 	limit = setLimit(limit, fileInfo)
+
+	// Check offset limits
+	err = checkOffset(offset, fileInfo)
+	if err != nil {
+		return err
+	}
 
 	// Create target file
 	targetFile, err := os.Create(toPath)
@@ -47,7 +78,7 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 	defer targetFile.Close()
 
-	// Offset
+	// Set offset
 	_, err = sourceFile.Seek(offset, io.SeekStart)
 	if errors.Is(err, io.EOF) {
 		err = nil
@@ -56,11 +87,20 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 		return err
 	}
 
+	pbLimit := progressBarLimit(fileInfo, limit, offset)
+	progressBar := pb.New(pbLimit)
+	progressBar.SetUnits(pb.U_BYTES)
+	progressBar.SetRefreshRate(time.Millisecond * 10)
+	progressBar.Start()
+
 	// Copy data
-	_, err = io.CopyN(targetFile, sourceFile, limit)
-	if err != nil {
+	reader := progressBar.NewProxyReader(sourceFile)
+	_, err = io.CopyN(targetFile, reader, limit)
+	if err != nil && !errors.Is(err, io.EOF) {
 		return err
 	}
+
+	progressBar.Finish()
 
 	return nil
 }

1fe3b9c6c99006ef17ea3b1e7886d645ae8b0e7d
Maxim Kursakov <qmax.ku@gmail.com>
2024-02-06 22:35:59 +0300

    [hw08] Add solved task, add tests

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,35 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	cmdEnv := make([]string, 0, len(cmd))
+	for name, value := range env {
+		if value.NeedRemove {
+			os.Unsetenv(name)
+			continue
+		}
+		cmdEnv = append(cmdEnv, fmt.Sprintf("%s=%s", name, value.Value))
+	}
+
+	cmdRun := exec.Command(cmd[0], cmd[1:]...)
+	cmdRun.Env = append(os.Environ(), cmdEnv...)
+	cmdRun.Stdout = os.Stdout
+	cmdRun.Stderr = os.Stderr
+	cmdRun.Stdin = os.Stdin
+
+	if err := cmdRun.Run(); err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode()
+		}
+		return 1
+	}
+	return 0
 }

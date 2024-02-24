fdf2d78ace1742131e66a287b52922e75c053125
Maxim Kursakov <qmax.ku@gmail.com>
2024-02-24 19:15:38 +0300

    [hw10] main task solved

---
+++ hw10_program_optimization/results/after.md
@@ -0,0 +1,8 @@
+```bash 
+=== RUN   TestGetDomainStat_Time_And_Memory
+    stats_optimization_test.go:46: time used: 220.268399ms / 300ms
+    stats_optimization_test.go:47: memory used: 13Mb / 30Mb
+--- PASS: TestGetDomainStat_Time_And_Memory (2.64s)
+PASS
+ok  	github.com/tog1s/hw-otus/hw10_program_optimization	2.643s
+```
--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,12 +1,13 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
 	"io"
 	"regexp"
 	"strings"
-	// "github.com/mailru/easyjson"
+
+	"github.com/mailru/easyjson"
 )
 
 type User struct {
@@ -32,15 +33,11 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 type users [100_000]User
 
 func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
+	scanner := bufio.NewScanner(r)
 
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
+	var user User
+	for i := 0; scanner.Scan(); i++ {
+		if err = easyjson.Unmarshal(scanner.Bytes(), &user); err != nil {
 			return
 		}
 		result[i] = user
@@ -51,11 +48,13 @@ func getUsers(r io.Reader) (result users, err error) {
 func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 
+	re, err := regexp.Compile("\\." + domain)
+	if err != nil {
+		return nil, fmt.Errorf("regexp compile error: %w", err)
+	}
+
 	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
-		}
+		matched := re.Match([]byte(user.Email))
 
 		if matched {
 			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
